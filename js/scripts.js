4145c65e082289f664c6f95674a38c693d9e25ac
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-07-06 17:42:56 -0400

    Execute a callback function

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,13 @@
 //Js Scripts
-alert("Test");
+    //Callback function example
+    function doSomething(callback) {
+        console.log("Doing something ...");
+
+        callback();
+    }
+
+    function callback() {
+        console.log(" executing callback function");
+    }
+
+doSomething(callback); //Pass callback function as object to doSomething function
