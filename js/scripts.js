1cdf4bd26bb6cdc7a944c81b2ec573171e947489
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-07-06 22:02:03 -0400

    Shorthand version of fizzbuzz

--- js/scripts.js
+++ js/scripts.js
@@ -100,13 +100,19 @@ function greet(param) {
 
   
   for(let i = 1; i <= 100; i++) {
-    if(i % 5 === 0 && i % 3 === 0) {
-        console.log("Fizzbuzz");
-    } else if (i % 5 === 0) {
-        console.log("Fizz");
-    } else if(i % 3 === 0) {
-        console.log("buzz");
-    } else {
-        console.log(i);
-    }
+    // if(i % 5 === 0 && i % 3 === 0) {
+    //     console.log("Fizzbuzz");
+    // } else if (i % 5 === 0) {
+    //     console.log("Fizz");
+    // } else if(i % 3 === 0) {
+    //     console.log("buzz");
+    // } else {
+    //     console.log(i);
+    // }
+
+    let f = i % 3 == 0,
+    b = i % 5 == 0;
+  console.log(f ? (b ? 'FizzBuzz' : 'Fizz') : b ? 'Buzz' : i);
   }
+
+  
