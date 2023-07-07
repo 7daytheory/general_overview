1c65148242eb38788f3b4df801c56055c5238ba5
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-07-06 21:38:38 -0400

    Check if an object is an array

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,19 @@
     }
 
 doSomething(callback); //Pass callback function as object to doSomething function
+
+
+//Check if object is an array or now
+var arrayList = [1 , 2, 3];
+
+function greet(param) {
+    if(Array.isArray(param)) {
+      // here have to check whether param is array or not
+      console.log("This is an array!");
+    }
+    else {
+        console.log("This is not an array");
+    }
+  }
+
+  greet(arrayList);
